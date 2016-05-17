43458289de446f14ce2c27a504221dd74db4ae86
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-17 18:32:39 -0500

    testing ajax api

--- index.php
+++ index.php
@@ -22,6 +22,34 @@
 
 
 <?php require_once('footer.php'); ?>
+<script>
+function request() {	
+	return $.ajax({
+		type:'GET',
+		dataType:'jsonp',
+		cache: false,
+
+
+	
+		url: "http://food2fork.com/api/search?key=41ea6ca476ba23e6554856b78d66c9d6&q=shredded%20chicken",
+		success: function(r) {
+			
+
+					var output = ' ';
+					$.each(r.results.slice(1,9), function(key, value){
+						console.log(value.title);
+						$("#app").append('<div class=col-md-4><p>' + value.title + '</p></div>');
+			
+						
+					});
+	
+				}
+	})
+}
+
+
+request();
+</script>
 
 </body>
 </html>
